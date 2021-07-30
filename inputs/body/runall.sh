208a5331b366ebe4d04d06e65d219e00e6285e7b
jkonak <jerenkonak2@gmail.com>
2021-07-29 23:40:52 -0400

    Remove File

--- inputs/body/runall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH -t 24:00:00
-#SBATCH -n 1
-#SBATCH --array=0-220
-YEAR_ID=1800
-#SBATCH -p large
-perl /home/fs01/nplmatchroot/nplmatch/inputs/body/terracenpl.pl bodynpl_digitized_lc.tsv > /home/fs01/nplmatchroot/nplmatch/inputs/body/bodybyrefyear/body_1802.tsv
